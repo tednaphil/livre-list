4ea9cc54eebaa8edf2a39f1d6cbbcbc59ff781da
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-18 18:42:29 -0500

    Add BookProfile comments and recommendations section element

--- src/BookProfile/BookProfile.tsx
+++ src/BookProfile/BookProfile.tsx
@@ -4,12 +4,14 @@ import { useEffect, useState } from 'react';
 import { Book } from '../Util/Interfaces';
 import { getBook } from '../Util/API_calls';
 import { Button } from '@chakra-ui/react';
+import Card from '../Card/Card';
 
 function BookProfile() {
     const id = useParams().id;
     const [book, setBook] = useState<Book | null>(null);
     // const [loading, setLoading] = useState(true);
     const [error, setError] = useState('');
+    const [recs, setRecs] = useState<Book[] | null>(null);
 
 
     useEffect(() => {
@@ -20,12 +22,15 @@ function BookProfile() {
       try {
         const data = await getBook(id);
         setBook(data);
+        //fetch reccommendations based on data.category[0]
       } catch(error: any) {
         setError(`There was a problem getting the book - ${error.message}`)
       }
     }
 
     const authors = book?.authors.map((author) => {
+      //if length === 1 return author with space at end
+      //if length > 1 return author with comma and space at end
       return (
         <p>{author}</p>
       )
@@ -34,12 +39,11 @@ function BookProfile() {
 
     return(
         <>
-          {/* <h2>Book Profile</h2> */}
-          {/* <h3>{id}</h3> */}
+          <div className='profile-wrapper'>
           <section className='book-profile'>
             <aside className='thumbnail-container'>
               <img src={book?.image_links.extraLarge} alt={`${book?.title} cover`}/>
-
+              <Button colorScheme='orange'>Add to Shelf</Button>
             </aside>
             <article className='book-info'>
               <h3>{book?.title}</h3>
@@ -48,9 +52,14 @@ function BookProfile() {
                 {authors}
                 <p>{`| ${book?.publisher} | Published: ${book?.published_date}`}</p>
               </section>
-              {book?.description}
+              <p>{book?.description}</p>
             </article>
           </section>
+          <section className='recs'>
+            <h4>Recommendations</h4>
+            {/* render reccommendation cards carousel here */}
+          </section>
+          </div>
         </>
     )
 }
