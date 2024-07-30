109db35b0e164a9ab9adb2fe01465d196830a4ac
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-07-30 17:33:07 -0500

    Add Shelf component padding

--- src/Shelf/Shelf.tsx
+++ src/Shelf/Shelf.tsx
@@ -4,10 +4,10 @@ import { useParams } from 'react-router-dom';
 function Shelf() {
   const shelfName = useParams().shelfname;
     return(
-        <>
+        <div className='shelf-wrapper'>
           <h2>Shelf</h2>
           <h3>{shelfName}</h3>
-        </>
+        </div>
     )
 }
 
