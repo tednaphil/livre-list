dc53adc61da79cb59a6ba8b3781add57e5885ea7
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-08-22 13:23:12 -0500

    Add loading and error page elements

--- src/Results/Results.tsx
+++ src/Results/Results.tsx
@@ -5,14 +5,17 @@ import { useState, useEffect } from 'react';
 import { getResults } from '../Util/API_calls';
 import Card from '../Card/Card';
 import SearchCtrl from '../SearchCtrl/SearchCtrl';
+import Loading from '../Loading/Loading';
+import ErrorPage from '../ErrorPage/ErrorPage';
 
 function Results() {
   const location = useLocation();
   const [searchTerm, setSearchTerm] = useState(location.state);
+  const term = useParams().term;
   const [results, setResults] = useState<Book[]>([]);
   const [sort, setSort] = useState<string>('ascending');
   const [filter, setFilter] = useState<string[] | null>(null);
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
   const sortResults = (books: Book[], orientation: string) => {
@@ -29,10 +32,8 @@ function Results() {
     } else {
 
     }
-
   }
 
-  
   const fetchData = async () => {
     try {
       const searchData = await getResults(searchTerm);
@@ -40,18 +41,19 @@ function Results() {
       //pass filtered data to sortResutls on line below
       const sortedData = sortResults(searchData, sort);
       setResults(sortedData);
+      setLoading(false)
     } catch(error: any) {
       setError(`There was a problem getting the search results - ${error.message}`)
+      setLoading(false)
     }
   }
   
   useEffect(() => {
     setError('')
     fetchData()
-  }, [sort])
+  }, [sort, term])
 
   const books = results?.map(book => {
-    // console.log('books map', book)
     return (
       <Card
         key={book.id}
@@ -66,16 +68,21 @@ function Results() {
 
     return(
         <>
+        {error && <ErrorPage error={error}/>}
+        {loading && <Loading/>}
         {/* <h2 className='results-header'>{`Search Results - ${searchTerm}`}</h2> */}
-        <h2 className='results-header'>{`Search Results - ${useParams().term}`}</h2>
-        <div className='results-container'>
-          <section className='sort-filter'>
-            <SearchCtrl setSort={setSort} setFilter={setFilter} filter={filter}/>
-          </section>
-          <section className='results-gallery'>
-            {books}
-          </section>
-        </div>
+        {!loading && 
+        <>
+          <h2 className='results-header'>{`Search Results - ${term}`}</h2>
+          <div className='results-container'>
+            <section className='sort-filter'>
+              <SearchCtrl setSort={setSort} setFilter={setFilter} filter={filter}/>
+            </section>
+            <section className='results-gallery'>
+              {books}
+            </section>
+          </div>
+        </>}
         </>
     )
 }
