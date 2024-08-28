3d752fdaf18ad89885cf2f5390c2cb843cdd5e07
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-08-28 17:44:28 -0500

    Add recommendations error assertion

--- cypress/e2e/error_handling.cy.ts
+++ cypress/e2e/error_handling.cy.ts
@@ -1,5 +1,10 @@
 describe('Error Handling Spec', () => {
   beforeEach(() => {
+    cy.intercept("GET", "https://5ed7ccd5-b752-4d30-bd73-96ddec3fba58.mock.pstmn.io/api/vi/users/106196942824430802445/bookshelves"
+      , {
+        statusCode: 200,
+        fixture: "shelves",
+      }).as('getShelves')
     cy.visit('http://localhost:3000')
   })
   it('Displays error page if page not found', () => {
@@ -24,11 +29,6 @@ describe('Error Handling Spec', () => {
       , {
         statusCode: 500,
       }).as('getRecsServerError') 
-    cy.intercept("GET", "https://5ed7ccd5-b752-4d30-bd73-96ddec3fba58.mock.pstmn.io/api/vi/users/106196942824430802445/bookshelves"
-      , {
-        statusCode: 200,
-        fixture: "shelves",
-      }).as('getShelves')
 
     cy.get('input[type="text"]').type('halloween{enter}')
     .get('.error-page').contains('There was a problem getting the search results - Couldn\'t get search results - 500')
@@ -41,25 +41,22 @@ describe('Error Handling Spec', () => {
       , {
         statusCode: 404,
       }).as('bookNotFound')
-    cy.intercept("GET", "https://5ed7ccd5-b752-4d30-bd73-96ddec3fba58.mock.pstmn.io/api/vi/users/106196942824430802445/bookshelves"
-      , {
-        statusCode: 200,
-        fixture: "shelves",
-      }).as('getShelves')
 
     cy.visit('http://localhost:3000/books/IJDQwQEACAAJ')
     .get('.error-page').contains('There was a problem getting the book data - Couldn\'t get the book - 404')
     })
-  // it('Displays error if book recommendations not received', () => {
-  //   cy.intercept("GET", "https://livre-list-be-c61f46345338.herokuapp.com/api/v1/books/IJDQwQEACAAJ"
-  //     , {
-  //       statusCode: 200,
-  //       fixture: 'book_response'
-  //     }).as('getBook') 
-  //   cy.intercept("GET", "https://livre-list-be-c61f46345338.herokuapp.com/api/v1/books?search=Juvenile%20Fiction%20/%20Holidays%20&%20Celebrations%20/%20Halloween"
-  //     , {
-  //       statusCode: 500,
-  //     }).as('getRecsServerError')
-  //   cy.visit('http://localhost:3000/books/IJDQwQEACAAJ')
-  // })
+  it('Displays error if book recommendations not received', () => {
+    cy.intercept("GET", "https://livre-list-be-c61f46345338.herokuapp.com/api/v1/books/IJDQwQEACAAJ"
+      , {
+        statusCode: 200,
+        fixture: 'book_response'
+      }).as('getBook') 
+    cy.intercept("GET", "https://livre-list-be-c61f46345338.herokuapp.com/api/v1/books?search=Juvenile%20Fiction%20/%20Holidays%20&%20Celebrations%20/%20Halloween"
+      , {
+        statusCode: 500,
+      }).as('getRecsServerError')
+
+    cy.visit('http://localhost:3000/books/IJDQwQEACAAJ')
+    .get('.recs').contains('Couldn\'t get recommendations - 500')
+    })
   })
