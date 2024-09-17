d83dad510b0c6278ee0aa94c03ac67bc2c392d59
Tayla Phillips <76406423+tednaphil@users.noreply.github.com>
2024-09-17 12:44:52 -0500

    Update interface values

--- src/Util/Interfaces.tsx
+++ src/Util/Interfaces.tsx
@@ -27,7 +27,7 @@ export interface AlertStatus {
 }
 
 export enum FilterValues {
-    purchaseable = 'purchaseable',
+    purchaseable = 'Purchaseable',
     fiction = 'Fiction',
     nonFiction = 'Nonfiction',
     childrens = 'Juvenile'
